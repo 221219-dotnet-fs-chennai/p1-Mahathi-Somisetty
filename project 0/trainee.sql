140cef9fa28f16cd048b7609a7a526724db58f3b
Mahathisomisetty <120800048+Mahathisomisetty@users.noreply.github.com>
2023-01-20 18:40:27 +0530

    Update trainee.sql

--- project 0/trainee.sql
+++ project 0/trainee.sql
@@ -7,45 +7,34 @@ Gender varchar(10),
 Age int not null, 
 PhoneNumber varchar(10), PRIMARY KEY([TraineeId])
 );
-CREATE table Companydeatails(
-CompanyId int not null,
-CompanyName varchar(30),
-ProjectName varchar(40),
-Position Varchar(30),
-Experience int not null,
-CId int,
-PRIMARY KEY([CompanyId]),
-CONSTRAINT[FK_Company.CId]
-FOREIGN KEY([CId])
-REFERENCES[TraineeDetails]([TraineeId])
-);
-Go
+select * from TraineeDetails;
 
-CREATE TABLE EducationalDetails(
-EducationId int not null,
-HQualification varchar(50),
-YearOfPassing date,
-[Percentage] float not null,
-[Stream] varchar(70),
-UG varchar(50),
-CONSTRAINT[FK_EducationalDetails.EducationId]
-FOREIGN KEY([EducationId])
-REFERENCES [TraineeDetails]([TraineeId])
+CREATE TABLE [Skills](
+    [Skill_id] int IDENTITY(1,1),
+    [Skill_name] VARCHAR(20),
+    [Skill_Type] VARCHAR(20),
+    [Expertise] VARCHAR(20),
+    CONSTRAINT [Fk_skill] FOREIGN KEY(Skill_id) REFERENCES TraineeDetails(TraineeId),
+    CONSTRAINT [Pk_skill] PRIMARY KEY([Skill_id]) 
 );
+select * from Skills;
 
-Go
-
-CREATE TABLE Skills(
-SkillID int not null,
-TechnologiesKnown varchar(70),
-ExpertiseInTechnology Varchar(50),
-[SID] int,
-PRIMARY KEY([SkillId]),
-CONSTRAINT[FK_Skills.skillId]
-FOREIGN KEY([SID])
-REFERENCES[TraineeDetails](TraineeId)
+CREATE TABLE [CompanyDetails](
+    [Id] int IDENTITY(1,1),
+    [Company_name] VARCHAR(30),
+    [ProjectName] VARCHAR(30),
+	[Position] Varchar(30),
+    [Experience] VARCHAR(20),
+    constraint [pk_company] PRIMARY KEY([Id]),
+    CONSTRAINT [Fk_company] FOREIGN KEY([Id]) REFERENCES TraineeDetails(TraineeId)
 );
-SELECT * FROM TraineeDetails
-SELECT * FROM Companydeatails
-SELECT * FROM EducationalDetails
-SELECT * FROM Skills
+select *from CompanyDetails;
+CREATE TABLE [EducationalDetails](
+    [EducationId] int IDENTITY(1,1),
+    [HQualification] VARCHAR(30),
+    [YearOfPassing] VARCHAR(50),
+    [Stream] VARCHAR(25),
+    CONSTRAINT [pk_education] PRIMARY KEY([Educationid]),
+    CONSTRAINT [Fk_education] FOREIGN KEY([Educationid]) REFERENCES TraineeDetails(Traineeid)
+);
+Select *from EducationalDetails;
