8a23f0bab8c182fa071736123a5c32be1ae47adc
Mahathisomisetty <120800048+Mahathisomisetty@users.noreply.github.com>
2023-01-21 10:34:02 +0530

    Update trainee.sql

--- project 0/trainee.sql
+++ project 0/trainee.sql
@@ -7,34 +7,41 @@ Gender varchar(10),
 Age int not null, 
 PhoneNumber varchar(10), PRIMARY KEY([TraineeId])
 );
-select * from TraineeDetails;
 
-CREATE TABLE [Skills](
-    [Skill_id] int IDENTITY(1,1),
+
+CREATE TABLE Skills(
+    [TraineeId] int IDENTITY(1,1),
     [Skill_name] VARCHAR(20),
     [Skill_Type] VARCHAR(20),
     [Expertise] VARCHAR(20),
-    CONSTRAINT [Fk_skill] FOREIGN KEY(Skill_id) REFERENCES TraineeDetails(TraineeId),
-    CONSTRAINT [Pk_skill] PRIMARY KEY([Skill_id]) 
+    CONSTRAINT [Fk_skill] FOREIGN KEY(TraineeId) REFERENCES TraineeDetails(TraineeId),
+    CONSTRAINT [Pk_skill] PRIMARY KEY([TraineeId]) 
 );
-select * from Skills;
+
 
 CREATE TABLE [CompanyDetails](
-    [Id] int IDENTITY(1,1),
+    [TraineeId] int IDENTITY(1,1),
     [Company_name] VARCHAR(30),
     [ProjectName] VARCHAR(30),
 	[Position] Varchar(30),
     [Experience] VARCHAR(20),
-    constraint [pk_company] PRIMARY KEY([Id]),
-    CONSTRAINT [Fk_company] FOREIGN KEY([Id]) REFERENCES TraineeDetails(TraineeId)
+    constraint [pk_company] PRIMARY KEY([TraineeId]),
+    CONSTRAINT [Fk_company] FOREIGN KEY([TraineeId]) REFERENCES TraineeDetails(TraineeId)
 );
-select *from CompanyDetails;
+
 CREATE TABLE [EducationalDetails](
-    [EducationId] int IDENTITY(1,1),
+    [TraineeId] int IDENTITY(1,1),
     [HQualification] VARCHAR(30),
     [YearOfPassing] VARCHAR(50),
     [Stream] VARCHAR(25),
-    CONSTRAINT [pk_education] PRIMARY KEY([Educationid]),
-    CONSTRAINT [Fk_education] FOREIGN KEY([Educationid]) REFERENCES TraineeDetails(Traineeid)
+    CONSTRAINT [pk_education] PRIMARY KEY([TraineeId]),
+    CONSTRAINT [Fk_education] FOREIGN KEY([TraineeId]) REFERENCES TraineeDetails(Traineeid)
 );
+
+
+select * from TraineeDetails;
 Select *from EducationalDetails;
+select *from CompanyDetails;
+
+
+select * from Skills;
